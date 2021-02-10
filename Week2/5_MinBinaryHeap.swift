776f3e0e8ecce76a0560df4a060ca1c143f15fbf
neo6 <525973044@qq.com>
2021-02-10 11:22:04 +0800

    Update 5_MinBinaryHeap.swift

--- Week2/5_MinBinaryHeap.swift
+++ Week2/5_MinBinaryHeap.swift
@@ -1,28 +1,34 @@
 
-/// 最小二叉堆
+/// 二叉堆
 
-class MinHeap<T: Comparable> {
+struct BinaryHeap<T: Comparable> {
+    
+    var isEmpty: Bool { array.isEmpty }
+    
+    var peek: T? { array.first }
     
     private var array: [T]
     
-    var minValue: T {
-        return array[0]
+    private let ordering: (T, T) -> Bool
+    
+    init(order: @escaping (T, T) -> Bool, items: [T]) {
+        ordering = order
+        array = []
+        for item in items {
+            add(item)
+        }
     }
     
-    init(items: [T]) {
-        self.array = items
-    }
-    
-    func insert(_ item: T) {
+    mutating func add(_ element: T) {
         
-        array.append(item)
+        array.append(element)
         
         heapifyUp()
     }
     
-    func deleteMinValue() -> T {
+    mutating func removePeek() -> T? {
         
-        let result = minValue
+        let result = peek
         
         array[0] = array[array.count - 1]
         
@@ -34,16 +40,15 @@ class MinHeap<T: Comparable> {
     }
 }
 
-private extension MinHeap {
+private extension BinaryHeap {
     
-    /// 数组末尾逐级向上与父亲节点比较，如果比父亲节点更小，则与父亲节点交换位置，直到自己比父亲节点更大或者已经到顶
-    func heapifyUp() {
+    mutating func heapifyUp() {
         
         let insertValue = array[array.count - 1]
         
         var i = array.count - 1
         
-        while i > 0, insertValue < array[parent(of: i)] {
+        while i > 0, ordering(insertValue, array[parent(of: i)]) {
             
             array.swapAt(i, parent(of: i))
             
@@ -51,18 +56,18 @@ private extension MinHeap {
         }
     }
     
-    /// 数组头部逐级向下找到最小的子节点与自己交换位置，直到子节点都比自己大或者已经到底
-    func heapifyDown() {
+    mutating func heapifyDown() {
+        
+        if array.isEmpty { return }
         
         let value = array[0]
         
         var i = 0
         
-        while leftChildIndex(of: i) < array.count, value > array[minChildIndex(of: i)] {
+        while leftChildIndex(of: i) < array.count,
+              let j = priorChildIndex(of: i), ordering(array[j], value) {
             
-            let j = minChildIndex(of: i)
-            
-            array.swapAt(i, minChildIndex(of: i))
+            array.swapAt(i, j)
             
             i = j
         }
@@ -78,11 +83,28 @@ private extension MinHeap {
         return (2 * index) + 1
     }
     
-    func minChildIndex(of index: Int) -> Int {
+    func priorChildIndex(of index: Int) -> Int? {
         
         let left = (2 * index) + 1
         let right = (2 * index) + 2
         
-        return (array[left] < array[right]) ? left : right
+        if left >= 0, left < array.count,
+           right >= 0, right < array.count {
+            return ordering(array[left], array[right]) ? left : right
+        }
+        if right < 0 || right >= array.count {
+            return left
+        }
+        if left < 0 || left >= array.count {
+            return right
+        }
+        
+        return nil
     }
+}
+
+extension BinaryHeap: CustomStringConvertible {
+    
+    var description: String { array.description }
+    
 }
